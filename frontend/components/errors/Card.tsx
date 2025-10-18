63c194daf79bc55c7fef9e1381131a2c5523da7b
Oscar Palm <palmoscar4@gmail.com>
2025-10-18 23:49:55 +0200

    Created a backend

--- frontend/components/errors/Card.tsx
+++ frontend/components/errors/Card.tsx
@@ -1,45 +1,45 @@
 import { useState } from 'react';
-import CardData from '../interfaces/CardData';
+import CardData from '../../interfaces/CardData';
 import styles from './Card.module.css';
 export interface CardProps {
-   cardProp: CardData;
-   filter: string;
+	cardProp: CardData;
+	filter: string;
 }
 
 const delayText = async (text: string | undefined, setText: any) => {
-   if (!text) return;
-   setTimeout(() => {
-      setText(text);
-   }, 350);
+	if (!text) return;
+	setTimeout(() => {
+		setText(text);
+	}, 350);
 };
 
 const checkFilter = (filter: string, cardInfo: CardData) => {
-   if (filter === '') return true;
-   return (
-      cardInfo.title.toLowerCase().includes(filter) ||
-      cardInfo.description.toLowerCase().includes(filter) ||
-      cardInfo.detailedDescription?.toLowerCase().includes(filter)
-   );
+	if (filter === '') return true;
+	return (
+		cardInfo.title.toLowerCase().includes(filter) ||
+		cardInfo.description.toLowerCase().includes(filter) ||
+		cardInfo.detailedDescription?.toLowerCase().includes(filter)
+	);
 };
 
 const Card = ({ cardProp, filter }: CardProps) => {
-   const [big, setBig] = useState(false);
-   const [text, setText] = useState('');
-   if (!checkFilter(filter, cardProp)) return null;
-   return (
-      <div
-         className={`cardClass ${big ? styles.bigCard : null}`}
-         onClick={() => {
-            setBig(!big);
-            big
-               ? setText('')
-               : delayText(cardProp.detailedDescription, setText);
-         }}
-      >
-         <h2 className={styles.cardTitle}> {cardProp.title}</h2>
-         <p> {cardProp.description}</p>
-         {big && cardProp.detailedDescription ? <p>{text}</p> : null}
-      </div>
-   );
+	const [big, setBig] = useState(false);
+	const [text, setText] = useState('');
+	if (!checkFilter(filter, cardProp)) return null;
+	return (
+		<div
+			className={`cardClass ${big ? styles.bigCard : null}`}
+			onClick={() => {
+				setBig(!big);
+				big
+					? setText('')
+					: delayText(cardProp.detailedDescription, setText);
+			}}
+		>
+			<h2 className={styles.cardTitle}> {cardProp.title}</h2>
+			<p> {cardProp.description}</p>
+			{big && cardProp.detailedDescription ? <p>{text}</p> : null}
+		</div>
+	);
 };
 export default Card;
--- frontend/pages/notes.tsx
+++ frontend/pages/notes.tsx
@@ -1,31 +1,49 @@
 import Head from 'next/head';
 import NotesCategory from '../components/notes/NotesCategory';
-import notes from '../data/notes.json';
+import { useState } from 'react';
+import { getFromApi } from './api/requests';
+
+type NoteCat = {
+	categoryName: string;
+	courses: {
+		name: string;
+		pdf: string;
+		image: string;
+	}[];
+};
 
 export default function Home() {
-   return (
-      <>
-         <Head>
-            <title>Lecture notes</title>
-            <meta
-               name="description"
-               content="Lookup table for notes from lectures"
-            />
-            <meta
-               name="viewport"
-               content="width=device-width, initial-scale=1"
-            />
-            <link rel="icon" href="/favicon.ico" />
-         </Head>
-         <main>
-            {notes.map((category) => (
-               <NotesCategory
-                  key={category.categoryName}
-                  categoryName={category.categoryName}
-                  notes={category.courses}
-               />
-            ))}
-         </main>
-      </>
-   );
+	const [notes, setNotes] = useState<NoteCat[]>();
+	getFromApi('sideprojects/notes/all')
+		.then((res) => setNotes(res))
+		.catch(() =>
+			console.log('Something went wrong fetching the notes from the backend')
+		);
+	return (
+		<>
+			<Head>
+				<title>Lecture notes</title>
+				<meta
+					name="description"
+					content="Lookup table for notes from lectures"
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				{notes
+					? notes.map((category) => (
+							<NotesCategory
+								key={category.categoryName}
+								categoryName={category.categoryName}
+								notes={category.courses}
+							/>
+						))
+					: ''}
+			</main>
+		</>
+	);
 }
