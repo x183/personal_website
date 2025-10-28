b737be5645b81c5f31ea2bbc7b85591a92f74f04
Oscar Palm <palmoscar4@gmail.com>
2025-10-28 23:01:45 +0100

    move projects

---
+++ frontend/pages/projects/index.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Oscar Palm</title>
+				<meta name="description" content="Created by Oscar Palm" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main className={styles.main}>
+				<h1> Welcome!</h1>
+			</main>
+		</>
+	);
+}
